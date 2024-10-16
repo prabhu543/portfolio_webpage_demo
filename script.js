66913af6d4ff9987913184678780ec68c420c9b4
Prabhu <prabhuprathamesh444@gmail.com>
2024-10-16 16:10:02 +0530

    portfoilo

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Initialize Typed.js
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('#element', {
+        strings: ['Full Stack Web Developer', 'Android App Developer', 'Machine Learning Learner'],
+        typeSpeed: 30,
+        loop: true
+    });
+});
+
+// Add any additional JavaScript functionality here
